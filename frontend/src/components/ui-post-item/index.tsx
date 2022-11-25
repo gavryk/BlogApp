c4ed845707fd34a4956146fdf2adb10ea79ba212
Gavryk <gvozdyk23@gmail.com>
2022-11-25 02:23:17 +0200

    ui: grid / post-item

--- frontend/src/components/ui-post-item/index.tsx
+++ frontend/src/components/ui-post-item/index.tsx
@@ -1,17 +1,30 @@
+import clsx from 'clsx';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { PostTypes } from '../../redux/slices/posts/types';
 import { UIButton } from '../ui-button';
 import styles from './styles.module.scss';
 
-export const UIPostItem: React.FC<PostTypes> = ({ id, title, desc, img }) => {
+type PostProp = PostTypes & {
+  variant?: 'row' | 'column';
+  direction?: 'normal' | 'reverse';
+};
+
+export const UIPostItem: React.FC<PostProp> = ({
+  id,
+  title,
+  desc,
+  img,
+  variant = 'row',
+  direction = 'normal',
+}) => {
   return (
-    <div className={styles.postItem}>
+    <div className={clsx(styles.postItem, styles[variant], styles[direction])}>
       <div className={styles.postImage}>
         <img src={img} alt={title} />
       </div>
-      <div className={styles.content}>
-        <h2>{title}</h2>
+      <div className={styles.postContent}>
+        <h3>{title}</h3>
         <p>{desc}</p>
         <Link to={`/post/${id}`}>
           <UIButton>Read More</UIButton>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { UIGrid } from '../components';
+import { UIPostItem } from '../components/ui-post-item';
+import { postsSelector } from '../redux/slices/posts/selectors';
 
 const Home: React.FC = () => {
+  const { posts } = useSelector(postsSelector);
+
   return (
     <div>
-      <h1>Home</h1>
+      {posts && (
+        <UIGrid columns={1} gridGap={20}>
+          {posts.map((post, index) => (
+            <UIPostItem
+              key={post.id}
+              {...post}
+              variant="row"
+              direction={index % 2 === 0 ? 'normal' : 'reverse'}
+            />
+          ))}
+        </UIGrid>
+      )}
     </div>
   );
 };
