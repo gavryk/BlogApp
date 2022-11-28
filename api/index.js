33a53367394a41c93afdd54e77fddefb2d0eef1d
Gavryk <gvozdyk23@gmail.com>
2022-11-28 15:16:13 +0200

    api: register user

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import authRoutes from './routes/auth.js';
-import userRoutes from './routes/user.js';
+import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
