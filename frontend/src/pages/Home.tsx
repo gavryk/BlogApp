01f41629bb8f71be0e15cb4aba445a66bfcc70d5
Gavryk <gvozdyk23@gmail.com>
2022-12-02 02:55:27 +0200

    redux: get posts, get it all or with category

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { UIGrid, UIPostItem, UITypography } from '../components';
+import { UIGrid, UIPostItem } from '../components';
 import { fetchPosts } from '../redux/slices/posts/asyncPosts';
 import { postsSelector } from '../redux/slices/posts/selectors';
-import { settingsSelector } from '../redux/slices/settings/selectors';
 import { useAppDispatch } from '../redux/store';
 
 const Home: React.FC = () => {
   const dispatch = useAppDispatch();
   const { posts, activeCat } = useSelector(postsSelector);
-  const { isLoaded } = useSelector(settingsSelector);
 
   useEffect(() => {
     dispatch(fetchPosts(activeCat));
