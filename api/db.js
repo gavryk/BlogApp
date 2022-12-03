0ad57158528722e4c49d27696b330a204ccc6e72
Gavryk <gvozdyk23@gmail.com>
2022-12-04 00:41:02 +0200

    api: .env file created

--- api/db.js
+++ api/db.js
@@ -4,10 +4,6 @@ import * as dotenv from 'dotenv';
 dotenv.config({ debug: true });
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1993099a',
-  database: 'blog',
   host: process.env.HOST_KEY,
   user: process.env.USER_KEY,
   password: process.env.PASSWORD_KEY,
